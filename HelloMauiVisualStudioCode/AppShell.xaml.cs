2d276bd9a7f24922dfbf0779e79f154d863efade
Phil Hadley <phadley@junipercommerce.com>
2024-09-13 20:22:43 -0600

    initial commit

---
+++ HelloMauiVisualStudioCode/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace HelloMauiVisualStudioCode;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ HelloMauiVisualStudioCode/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace HelloMauiVisualStudioCode;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
